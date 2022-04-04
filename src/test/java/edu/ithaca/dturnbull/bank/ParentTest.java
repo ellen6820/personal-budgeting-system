0f5dc74cdb73f8bd3c516d31eae362bd3d4379be
Laci Young <lyoung4@ithaca.edu>
2022-04-03 23:35:10 -0400

    edit tests

--- src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ParentTest{
     @Test
     void addChildTest(){
-        User user = new User(0,null,null,0,null);
+
         Parent parent = new Parent();
 
-        parent.addChild(0,null,null,10,"kid@gmail.com");
+        Child child = parent.addChild(0,null,null,10,"kid@gmail.com");
 
         assertEquals(10, child.getIncome()); //Tests if child exists by seeing if the income 
                                              //matches the value passed in while adding a child
@@ -21,15 +21,13 @@ public class ParentTest{
     @Test
     void setLimitTest(){
 
-        User user = new User(0,null,null,0,null);
-
         Parent parent = new Parent();
 
-        parent.addChild(60,null,null,0,null);
+        Child child = parent.addChild(60,null,null,0,null);
 
         parent.setLimit(50);
 
-        assertTrue(child.getLimit(), 50); //Tests that value of limit for child is what was set by parent
+        assertEquals(child.getLimit(), 50); //Tests that value of limit for child is what was set by parent
 
     }
 
