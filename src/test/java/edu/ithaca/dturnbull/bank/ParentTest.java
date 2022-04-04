3147c066fbc8172dfea2011491b7f37d45b84afb
Laci Young <lyoung4@ithaca.edu>
2022-04-03 23:08:58 -0400

    setLimitTest

--- src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
@@ -7,10 +7,11 @@ public class ParentTest{
     @Test
     void addChildTest(){
         User user = new User(0,null,null,0,null);
+        Parent parent = new Parent();
 
-        user.addChild(0,null,null,10,"kid@gmail.com");
+        parent.addChild(0,null,null,10,"kid@gmail.com");
 
-        assertEquals(child.getIncome(), 10); //Tests if child exists by seeing if the income 
+        assertEquals(10, child.getIncome()); //Tests if child exists by seeing if the income 
                                              //matches the value passed in while adding a child
 
         assertEquals(child.getEmail(), "kid@gmail.com"); //Tests if child exists by seeing if the email
@@ -20,6 +21,15 @@ public class ParentTest{
     @Test
     void setLimitTest(){
 
+        User user = new User(0,null,null,0,null);
+
+        Parent parent = new Parent();
+
+        parent.addChild(60,null,null,0,null);
+
+        parent.setLimit(50);
+
+        assertTrue(child.getLimit(), 50); //Tests that value of limit for child is what was set by parent
 
     }
 
