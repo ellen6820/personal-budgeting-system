1aa3a130d1d1210ba01cef6ddec2083c45b0a9c5
Laci Young <lyoung4@ithaca.edu>
2022-04-03 22:14:40 -0400

    addChildTest

--- src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/ParentTest.java
@@ -3,12 +3,18 @@ package edu.ithaca.dturnbull.bank;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ParentTest {
-    
+public class ParentTest{
     @Test
     void addChildTest(){
+        User user = new User(0,null,null,0,null);
 
-        
+        user.addChild(0,null,null,10,"kid@gmail.com");
+
+        assertEquals(child.getIncome(), 10); //Tests if child exists by seeing if the income 
+                                             //matches the value passed in while adding a child
+
+        assertEquals(child.getEmail(), "kid@gmail.com"); //Tests if child exists by seeing if the email
+                                                         //matches the value passed in while adding a child
     }
 
     @Test
