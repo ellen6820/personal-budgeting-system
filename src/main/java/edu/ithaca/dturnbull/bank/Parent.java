d5c91f0a9a8e5593181ac6c8078dd060a7802a06
Laci Young <lyoung4@ithaca.edu>
2022-04-06 14:45:01 -0400

    edited addChild and impemented seeHistory

--- src/main/java/edu/ithaca/dturnbull/bank/Parent.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Parent.java
@@ -1,5 +1,7 @@
 package edu.ithaca.dturnbull.bank;
 
+import java.util.ArrayList;
+
 //import java.lang.reflect.Array;
 
 public class Parent extends User {
@@ -15,11 +17,13 @@ public class Parent extends User {
 
         Child child = new Child(balance,weeklyLimit,email);
 
+        children.add(child);
+
         return child;
 
     }
     
-    void setChildLimit(Child child,double limit){
+    void setChildLimit(Child child, double limit){
 
         child.weeklyLimit = limit;
 
@@ -30,8 +34,18 @@ public class Parent extends User {
 
     }
     
-    void seeChildHistory(){
+    ArrayList<Child> seeChildHistory(){
 
+        ArrayList<Child> children = new ArrayList<Child>();
 
+        for (int i = 0; i < children.size(); i++){
+
+            System.out.println(children.get(i).getBalance());
+            System.out.println(children.get(i).getLimit());
+            System.out.println(children.get(i).getEmail());
+
+        }
+        
+        return children;
     }
 }
