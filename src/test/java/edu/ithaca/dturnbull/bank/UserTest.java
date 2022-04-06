46d473701138d9cb8cd6734443cbb9a25a69c226
ellen6820 <echapman@ithaca.edu>
2022-04-06 11:20:22 -0400

    user tests createGoals and seeHistory

--- src/test/java/edu/ithaca/dturnbull/bank/UserTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/UserTest.java
@@ -22,6 +22,13 @@ public class UserTest {
 
         @Test
         public void seeHistoryTest(){
+                User user1 = new User(0, null, null, 0, null);
+                user1.createTransaction("Shopping", 10); //needs date
+                user1.createTransaction("Food", 50); //needs date
+
+                assertEquals(user1.seeHistory(), "Last 5 Transactions: \n Shopping $10 \n Food $50");
             
+                assertThrows(IllegalArgumentException.class, () -> user1.createTransaction("Savings", -90)); //negative amount
+                assertThrows(IllegalArgumentException.class, () -> user1.createTransaction("Savings", 1000000000)); // more than balance
         }
 }
