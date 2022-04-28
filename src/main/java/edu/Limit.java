c43026c31bcc8180b70d9ce9bbe1123e42b26219
Laci Young <lyoung4@ithaca.edu>
2022-04-28 11:53:13 -0400

    Created limit stubs and limitTest class

--- src/main/java/edu/Limit.java
+++ src/main/java/edu/Limit.java
@@ -14,13 +14,25 @@ public class Limit {
     public String getLimitType(){
         return limitType;
     }
-    
+
     public double getAmount(){
         return amount;
     }
 
     public double getAmountSpent(){
         return amountSpent;
+    }
+
+    public void resetLimit(){
+
+    }
+
+    public void addSpendingAmount(){
+
+    }
+
+    public void overspendingLimit(){
+        
     }
 
 }
--- src/test/LimitTest.java
+++ src/test/LimitTest.java
@@ -1,3 +1,23 @@
-public class LimitTest {
+package edu.ithaca.dturnbull.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LimitTest{
+    @Test
+    public void resetLimitTest(){
+
+    }
+
+    @Test
+    public void addSpendingAmountTest(){
+
+    }
+
+    @Test
+    public void overspendingLimitTest(){
+
+    }
+
     
 }
