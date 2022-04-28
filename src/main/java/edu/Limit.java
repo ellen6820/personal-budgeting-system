403cc1481637932cb76aacb0390f84d53d9524ce
Laci Young <lyoung4@ithaca.edu>
2022-04-28 11:26:40 -0400

    created Limit class

---
+++ src/main/java/edu/Limit.java
@@ -0,0 +1,26 @@
+package edu;
+
+public class Limit {
+    public String limitType;
+    public double amount;
+    public double amountSpent;
+
+    public Limit(String limitType,double amount, double amountSpent){
+        this.limitType = limitType;
+        this.amount = amount;
+        this.amountSpent = amountSpent;
+    }
+
+    public String getLimitType(){
+        return limitType;
+    }
+    
+    public double getAmount(){
+        return amount;
+    }
+
+    public double getAmountSpent(){
+        return amountSpent;
+    }
+
+}
