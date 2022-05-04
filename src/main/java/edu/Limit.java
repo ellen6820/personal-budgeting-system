123b7b6cbf362a487046df542eae6919afc614e8
ellen6820 <echapman@ithaca.edu>
2022-05-03 22:40:16 -0400

    documentation

--- src/main/java/edu/Limit.java
+++ src/main/java/edu/Limit.java
@@ -23,14 +23,18 @@ public class Limit {
         return amountSpent;
     }
 
+    //the limit will reset to no money have being spent for the current time period
     public void resetLimit(){
         
     }
 
+    //a transaction a user creates that applies to a limit affects the amount left in that limit for the time period
     public void addSpendingAmount(){
 
     }
 
+    /*if a user attempts to log a transaction that results in their spending for the time period
+        going over the limit for the time period, they will be notified*/
     public void overspendingLimit(){
 
     }
--- src/test/LimitTest.java
+++ src/test/LimitTest.java
@@ -7,18 +7,22 @@ public class LimitTest{
 
     @Test
     public void resetLimitTest(){
-        //the limit will reset to no money have being spent for the current time period
+        //limit with no spending tracked
+        //full limit
+        //overspent limit
     }
 
     @Test
     public void addSpendingAmountTest(){
-        //a transaction a user creates that applies to a limit affects the amount left in that limit for the week
+        //transaction in the limit category 
+        //transaction not in the limit category
+        
     }
 
     @Test
     public void overspendingLimitTest(){
-        /*if a user attempts to log a transaction that results in their spending for the time period
-        going over the limit for the time period, they will be notified*/
+        //limit is full
+        //limit is not full
     }
 
     
